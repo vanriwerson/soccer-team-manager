d49a648ea9ad97b3a7730f8129179490ff47fc56
Bruno Riwerson Silva <riwerson@gmail.com>
2022-08-20 19:52:55 -0300

    Using POST

--- src/app.js
+++ src/app.js
@@ -1,5 +1,33 @@
 const express = require('express');
 
+const teams = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SPF',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+];
+
+const OK = 200;
+const CREATED = 201;
+
 const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => res.status(OK).json({ message: 'Olá Mundo!' }));
+app.get('/teams', (req, res) => res.status(200).json({ teams }));
+
+app.post('/teams', (req, res) => {
+  const newTeam = { ...req.body };
+  teams.push(newTeam);
+
+  res.status(CREATED).json({ team: newTeam });
+});
 
 module.exports = app;
